738644e5e59ba8d98c12838b9030ae4ec96161b1
vgutta <Vineeth@Vineeths-MacBook-Pro.local>
2018-01-30 19:03:12 -0500

    pushing all files

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Hello from './components/hello';
+
+//import registerServiceWorker from './registerServiceWorker';
+
+// const root = <div>
+//     <Hello name="World" />
+//     <Hello name="New World" />
+// </div>
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
